0af94a4cccbc078a4d1861a06ab243b082071af5
valen2510 <valenjaramillo1025@hotmail.com>
2020-10-18 07:04:36 +0000

    Function to sort a double linked list of integers in ascending order [Insertion sort algorithm]

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - function that sorts a doubly linked list of
+ * integers in ascending order using the Insertion sort algorithm.
+ * @list: double linked list.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *previous;
+
+	if (!(*list)->next || !(*list)->next->next)
+		return;
+	/**
+	 * First loop to transverse the double linked list
+	 **/
+	for (current = (*list)->next; current; current = current->next)
+	{
+		/**
+		* Second loop to compare the integers on the current position
+		* with the previuos one if exist
+		**/
+		while (current->prev && current->prev->n > current->n)
+		{
+			/**
+			* If the condition is True, then the nodes switch places
+			**/
+			previous = current->prev;
+			current->prev = previous->prev;
+			previous->prev = current;
+			previous->next = current->next;
+			current->next = previous;
+			/**
+			* Condition to check in case of current node reach the first position
+			**/
+			if (current->prev)
+				current->prev->next = current;
+			else
+				*list = current;
+			/**
+			* Condition in case the previous node reach the last position
+			**/
+			if (previous->next)
+				previous->next->prev = previous;
+			print_list(*list);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -4,12 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_array(const int *array, size_t size);
-/*void print_list(const listint_t *list);*/
-void bubble_sort(int *array, size_t size);
-/*void insertion_sort_list(listint_t **list);*/
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -24,5 +18,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
